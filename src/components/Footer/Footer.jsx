76b6656ecac3ba0b90565b20d115678cb6b54faf
Леонид <lenya.zverevich@gmail.com>
2023-09-20 13:52:35 +0300

    add adaptive design up to 320px

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,12 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer__row">
-        <Link to={"/home"}>
+        <Link
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+          to={"/home"}
+        >
           <Logo fill={"#ffffff"} />
         </Link>
         <div className="footer__desc">
